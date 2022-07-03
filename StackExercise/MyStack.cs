b4ea2551781c4887137a008774590c2555c53ed2
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-07-03 19:16:34 +0300

    Set up stack structure using list structure

---
+++ StackExercise/MyStack.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace StackExercise;
+
+public class MyStack<T>
+{
+    private List<T> _myStack;
+
+    public MyStack()
+    {
+        _myStack = new List<T>();
+    }
+}
